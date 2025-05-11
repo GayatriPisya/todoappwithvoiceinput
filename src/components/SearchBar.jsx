82e4e80efc67de02957260907bcd2a7107edd1f1
Gayatri Pisya <gayatripisya@gmail.com>
2025-05-11 21:17:54 +0530

    updated

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,22 @@
+// components/SearchBar.jsx
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSearch } from '@fortawesome/free-solid-svg-icons'; 
 
 const SearchBar = ({ searchTerm, setSearchTerm }) => {
   return (
-    <input type="text" className="form-control w-50" placeholder="Search tasks..." value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+    <div className="input-group w-50">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Search tasks..."
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <span className="input-group-text">
+        <FontAwesomeIcon icon={faSearch} />
+      </span>
+    </div>
   );
 };
 
